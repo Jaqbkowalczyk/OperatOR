9d1dda3191d512c530d351f66f9556f9bc7aef09
Jaqbkowalczyk <jaqb.kowalczyk@gmail.com>
2021-04-21 11:17:03 +0200

    add copydocxtemplate and createparcelfile functions

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import os
 import re
 import logging
 import pyautogui
+import win32com.client as win32
 from docx import Document
 import tkinter as tk
 from tkinter.filedialog import askdirectory, askopenfile
-
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
+from docx.table import Table
+from docx.text.paragraph import Paragraph
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 pyautogui.FAILSAFE = True
@@ -39,11 +43,36 @@ def open_folder():
     return filename
 
 
+def ConvertRtfToDocx(rootDir, file):
+    word = win32.Dispatch("Word.Application")
+    wdFormatDocumentDefault = 16
+    wdHeaderFooterPrimary = 1
+    doc = word.Documents.Open(rootDir + "\\" + file)
+    for pic in doc.InlineShapes:
+        pic.LinkFormat.SavePictureWithDocument = True
+    for hPic in doc.sections(1).headers(wdHeaderFooterPrimary).Range.InlineShapes:
+        hPic.LinkFormat.SavePictureWithDocument = True
+    doc.SaveAs(str(rootDir + "\\" + file + ".docx"), FileFormat=wdFormatDocumentDefault)
+    doc.Close()
+    word.Quit()
+
+
 def get_text_from_doc(filename):
     doc = Document(filename)
     fullText = []
+    i = 0
+    j = 0
+    print(len(doc.sections))
     for para in doc.paragraphs:
         fullText.append(para.text)
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                print(cell.text)
+                j += 1
+            i += 1
+        i = 0
+        j = 0
     return '\n'.join(fullText)
 
 
@@ -86,22 +115,75 @@ def write_report():
             paragraph.text = 'new text containing ocean'
 
 
+def createparcelfile(file):
+    """This function creates parcel text file from .docx table with parcels"""
+    doc = Document(file)
+    i = 0
+    j = 0
+    table = doc.tables[0]
+    output = open(os.getcwd() + '\\parcels.txt', 'w')
+    print(os.getcwd() + '\\parcels.txt')
+    for row in table.rows:
+        if i < 2:
+            i += 1
+            continue
+        for cell in row.cells:
+            if j == 1:
+                parcels = cell.text.split(',')
+                newline = lambda x: x + '\n'
+                for parcel in parcels:
+                    parcel = parcel.replace(' ', '')
+                    output.write(newline(parcel))
+            j += 1
+        i += 1
+        j = 0
+    i = 0
+    output.close()
+    return output
 
+
+def copydocxtemplate(templatefile, outputfile):
+    """function that copies docx template into an end of output file"""
+    # select only paragraphs or table nodes
+    template = Document(templatefile)
+    get_text_from_doc(templatefile)
+    output = Document()
+    for child in template.element.body.xpath('w:p | w:tbl'):
+        if isinstance(child, CT_P):
+            paragraph = Paragraph(child, template)
+            outpara = output.add_paragraph()
+            outpara._p.addnext(paragraph._p)
+        elif isinstance(child, CT_Tbl):
+            table = Table(child, template)
+            paragraph = output.add_paragraph()
+            paragraph._p.addnext(table._tbl)
+    output.save(outputfile)
+
+class Zawiadomienie:
+    def __init__(self, name, surname, address, hour, date, type):
+        self.name = name
+        self.surname = surname
+        self.address = address
+        self.hour = hour
+        self.date = date
+        self.type = type
 
 
 def main():
     """ Main program """
-    x = 400
+    """x = 400
     y = 200
-    pyautogui.moveTo(x, y)
-    check_project_data()
-    write_report()
-
+    pyautogui.moveTo(x, y)"""
+    #check_project_data()
+    #write_report()
+    #ConvertRtfToDocx('C:\\Users\\Jurek\\Documents\\Kuba\\Python\\OperatOR\\docs','info_o_materiałach1.rtf')
+    #print(createparcelfile('C:\\Users\\Jurek\\Dysk Google\\GEO\\Bibice_Zbożowa\\Wyznaczenie\\protokol_wyznaczenia_granic.docx'))
+    copydocxtemplate('C:\\Users\\Jurek\\Dysk Google\\GEO\\Bibice_Zbożowa\\Wyznaczenie\\protokol_wyznaczenia_granic.docx', 'out.docx')
     return 0
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    """root = tk.Tk()
     root.withdraw()
 
     toplevel = tk.Toplevel(root)
@@ -111,7 +193,7 @@ if __name__ == "__main__":
     menubar.add_command(label="Hello!")
     menubar.add_command(label="Quit!", command=root.quit)
     # display the menu
-    toplevel.config(menu=menubar)
+    toplevel.config(menu=menubar)"""
     main()
-    root.mainloop()
+    #root.mainloop()
 
